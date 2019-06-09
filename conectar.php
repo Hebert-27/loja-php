3d1a24d2921fd998c57138fd7889e2005ac98a7b
Hebert Nunes <42046520+Hebert-27@users.noreply.github.com>
2019-06-09 14:24:42 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -4,7 +4,7 @@
  * Método responsável por favor a conexão no banco de dados
  */
 function conectar(){
-$conexao = mysqli_connect("localhost", "root", "", "loja");
+$conexao = mysqli_connect("	s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "inw4t7uz1z4u7d2z", "kn0p05f74mzvil15", "owwpgdlhr9qppan1");
 
 return $conexao;
 }
